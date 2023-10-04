f1829b88a9f8c685de8607070c6523b6dc8a653d
matija.djurdjakovic <matija.djurdjakovic@helbiz.com>
2023-10-04 16:27:33 +0200

    test commit 2

--- src/IntegerDouble.java
+++ src/IntegerDouble.java
@@ -3,7 +3,7 @@ public class IntegerDouble {
          //int x = 113.0/112.0;
          double x = 113.0/112;
         //System.out.println(x);
-        System.out.printf("zaokruzi broj na 7 decimala %.15f", x);
+        System.out.printf("zaokruzi broj na 7 decimala %.7f", x);
 
     }
 }
