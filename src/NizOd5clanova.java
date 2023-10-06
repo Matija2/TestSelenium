660372b867eda771f512bb6f8568e5ef94e7c3f7
matija.djurdjakovic <matija.djurdjakovic@helbiz.com>
2023-10-06 18:20:05 +0200

    Niz od 5 clanova

---
+++ src/NizOd5clanova.java
@@ -0,0 +1,19 @@
+public class NizOd5clanova {
+    public static void main(String[] args) {
+        int max = 0;
+        int mojNiz[] = new int[5];
+        mojNiz[0] = 1;
+        mojNiz[1] = 3;
+        mojNiz[2] = 5;
+        mojNiz[3] = 2;
+        mojNiz[4] = 7;
+
+        for (int i = 0; i < mojNiz.length; i++) {
+            if (mojNiz[i] > max) {
+                max = mojNiz [i];
+            }
+        }
+        System.out.println(max);
+
+    }
+}
