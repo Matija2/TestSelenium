d3ba747560277bc4399b851f1fbaf1ab38eeae82
matija.djurdjakovic <matija.djurdjakovic@helbiz.com>
2023-10-10 12:51:50 +0200

    task3

--- src/Task3.java
+++ src/Task3.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
         /* Napisati program koji pomaže kasirki da obračuna kusur
         koji treba da vrati kupcu. Za unetu cenu artikla, količinu artikla i iznos koji je
         kupac dao, program treba da ispiše vrednost kusura. Sve ulazne vrednosti su
         pozitivni celi brojevi. Napomena: Pretpostaviti da je unos ispravan.*/
+
+        Scanner skener = new Scanner(System.in);
+        System.out.println("Ubacite cenu artikla: ");
+        int cenaArtikla = skener.nextInt();
+        System.out.println("Ubacite kolicinu artikla: ");
+        int kolicinaArtikla = skener.nextInt();
+        System.out.println("Ubacite iznos koji je kupac dao za placanje: ");
+        int iznos = skener.nextInt();
+        int kusur = iznos - (cenaArtikla * kolicinaArtikla);
+        System.out.println("Vrednost kusura je: " + (kusur));
+
+        //sve ovo moze i u double-u da se uradi
+
     }
 }
