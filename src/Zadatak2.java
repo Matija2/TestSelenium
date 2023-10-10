b27b78a8881a16d2666c63a30d95f354d6b824b0
matija.djurdjakovic <matija.djurdjakovic@helbiz.com>
2023-10-10 15:42:26 +0200

    nesto drugo

--- src/Zadatak2.java
+++ src/Zadatak2.java
@@ -1,6 +1,6 @@
 public class Zadatak2 {
     public static void main(String[] args) {
         // Your code out folder ce uvek ignorisati
-//        lamsdla
+//        lamsdla ,jasndklasnlkdajad
     }
 }
