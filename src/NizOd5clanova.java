aa86a467ff9a2e88ecb694912a7a15dc15cb2869
matija.djurdjakovic <matija.djurdjakovic@helbiz.com>
2023-10-10 14:42:18 +0200

    test za ignore out foldera

--- src/NizOd5clanova.java
+++ src/NizOd5clanova.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class NizOd5clanova {
 
     public static void main(String[] args) {
-        int max = 0;
         int mojNiz[] = new int[5];
         mojNiz[0] = 1;
         mojNiz[1] = 3;
         mojNiz[2] = 5;
         mojNiz[3] = 2;
         mojNiz[4] = 7;
+        int max = mojNiz[0];
 
         for (int i = 0; i < mojNiz.length; i++) {
             if (mojNiz[i] > max) {
@@ -15,6 +17,9 @@ public class NizOd5clanova {
             }
         }
         System.out.println(max);
+        Arrays.sort(mojNiz);
+        System.out.println("Sortiran niz: " + Arrays.toString(mojNiz));
+
 
     }
 }
